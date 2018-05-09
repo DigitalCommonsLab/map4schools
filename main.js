9622320968ebf747cdc3e12da5149cb640c6d795
Stefano Cudini <stefano.cudini@gmail.com>
2018-05-09 17:23:48 +0200

    added regions, leflet draw

--- main.js
+++ main.js
@@ -4,18 +4,19 @@ var $ = require('jquery'),
 	s = require('underscore.string'),
 	csv = require('jquery-csv'),
 	L = require('leaflet'),
-	Search = require('leaflet-search');
+	Search = require('leaflet-search'),
+	Select = require('leaflet-geojson-selector');
 	//dissolve = require('geojson-dissolve');
 	//Panel = require('leaflet-panel-layers');
 
 _.mixin({str: s});
 
 //TODO test browsrify-css
-var btcss = require('./node_modules/bootstrap/dist/css/bootstrap.min.css'),
-	lfcss = require('./node_modules/leaflet/dist/leaflet.css'),
-	lscss = require('./node_modules/leaflet-search/dist/leaflet-search.min.css');
-
-console.log(btcss,lfcss,lscss);
+require('./node_modules/bootstrap/dist/css/bootstrap.min.css'),
+require('./node_modules/leaflet/dist/leaflet.css'),
+require('./node_modules/leaflet-search/dist/leaflet-search.min.css');
+require('./node_modules/leaflet-geojson-selector/dist/leaflet-geojson-selector.min.css');
+require('./node_modules/leaflet.draw/src/leaflet.draw.css');
 
 function randomColor(str) {
 	var letters = '0123456789ABCDEF';
@@ -28,14 +29,35 @@ function randomColor(str) {
 
 $(function() {
 
-	var map = new L.Map('map', {
-		zoom: 15,
-		center: new L.latLng([46.07,11.13]),
-		layers: L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-		})
-	});
+var map = new L.Map('map', {
+	zoom: 15,
+	center: new L.latLng([46.07,11.13]),
+	layers: L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	})
+});
 
+$.getJSON('data/italy-regions.json', function(json) {
+
+		geoLayer = L.geoJson(json).addTo(map);
+
+		var geoSelect = new L.Control.GeoJSONSelector(geoLayer, {
+			zoomToLayer: true,
+			listOnlyVisibleLayers: true
+		}).on('change', function(e) {
+			
+			console.log( e.layers[0].feature.properties.name );
+
+		}).addTo(map);
+
+		map.setMaxBounds( geoLayer.getBounds().pad(0.5) );
+
+		map.fitBounds(geoLayer.getBounds());
+
+		map.on('click', function(e) {
+			map.fitBounds(geoLayer.getBounds())
+		});
+});
 /*
 	L.control.search({
 		layer: geo,
